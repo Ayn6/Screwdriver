e2978950c3b397942d934f45490db2f8b20f9856
Анна <anacetkareva@gmail.com>
2024-06-29 10:53:11 +0400

    Мои скрипты

---
+++ Assets/ScriptabelsObjects/Plants/Ingridient.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Ingridient")]
+public class Ingridient : ScriptableObject
+{
+    public int count = 0;
+    public string name;
+    public Sprite sprite;
+}
+
